671fb8edbd71809db3196b96698a1fb7219ef2d1
bbrolezzi <bbrolezzi@users.noreply.github.com>
2020-07-23 10:05:57 +0100

    didn't understand why the random beer didn't show up

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const { request } = require('http');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
@@ -13,13 +14,31 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+app.get('/beers', (request, response) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi);
+      response.render('beers', { beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+app.get('/random-beers', (request, response) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      // your magic happens here
+      console.log('Random beers: ', responseFromAPI);
+      response.render('random-beers.hbs', { responseFromAPI });
+    })
+    .catch(error => console.log(error));
+});
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index.hbs');
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
